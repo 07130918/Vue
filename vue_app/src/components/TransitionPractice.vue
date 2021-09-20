9c41721955d9a200773d2929ed3a1bb5675a081e
こた <tokyosato@gmail.com>
2021-09-20 09:24:21 +0900

    slide

--- vue_app/src/components/TransitionPractice.vue
+++ vue_app/src/components/TransitionPractice.vue
@@ -5,6 +5,9 @@
             <h4 v-if="show">Hello</h4>
         </transition>
         <button @click="show = !show">切り替え</button>
+        <transition name="slide">
+            <p v-if="show">bye</p>
+        </transition>
     </div>
 </template>
 
@@ -36,6 +39,26 @@ export default {
     }
     .fade-leave-to { /*消えるときの最後の状態*/
         opacity: 0;
+    }
+    .slide-enter, .slide-leave-to {
+        opacity: 0;
+    }
+    .slide-enter-active{
+        animation: slide-in 0.5s;
+        transition: opacity 1s;
+    }
+    .slide-leave-active{
+        animation: slide-in 0.5s reverse;
+        transition: opacity 1s;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateX(100px);
+        }
+        to {
+            transform: translateX(0);
+        }
     }
     .main {
         width: 70%;
