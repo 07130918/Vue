b2247ffd49bca702366b02ae1e72cb1ecdf72c6b
こた <tokyosato@gmail.com>
2021-09-20 12:02:01 +0900

    scale circle

--- vue_app/src/components/TransitionPractice.vue
+++ vue_app/src/components/TransitionPractice.vue
@@ -5,18 +5,7 @@
         <p>{{myAnimation}}</p>
         <button @click="show = !show">切り替え</button>
         <br><br>
-        <transition
-            :css="false"
-            @before-enter="beforeEnter"
-            @enter="enter"
-            @after-enter="afterEnter"
-            @enter-cancelled="enterCancelled"
-
-            @before-leave="beforeLeave"
-            @leave="leave"
-            @after-leave="afterLeave"
-            @leave-cancelled="leaveCancelled"
-        >
+        <transition :css="false" @before-enter="beforeEnter" @enter="enter" @leave="leave">
             <div class="circle" v-if="show"></div>
         </transition>
         <br>
@@ -61,29 +50,45 @@ export default {
     },
     methods: {
         beforeEnter(el) { //現れる前
-
+            el.style.scale = 'scale(0)'
         },
         enter(el, done) { //現れるとき
-
+            let scale = 0;
+            const interval = setInterval(() => {
+                el.style.transform = `scale(${scale})`
+                scale += 0.1
+                if (scale > 1) {
+                    clearInterval(interval);
+                    done();
+                }
+            }, 10)
         },
-        afterEnter(el) { //現れた後
+        // afterEnter(el) { //現れた後
 
-        },
-        enterCancelled(el) { //現れるアニメーションがキャンセルされた時
+        // },
+        // enterCancelled(el) { //現れるアニメーションがキャンセルされた時
 
-        },
-        beforeLeave(el) { //消える前
+        // },
+        // beforeLeave(el) { //消える前
 
-        },
+        // },
         leave(el, done) { //消えるとき
-
+            let scale = 1;
+            const interval = setInterval(() => {
+                el.style.transform = `scale(${scale})`
+                scale -= 0.1
+                if (scale < 0) {
+                    clearInterval(interval);
+                    done();
+                }
+            }, 10)
         },
-        afterLeave(el) { //消えた後
+        // afterLeave(el) { //消えた後
 
-        },
-        leaveCancelled(el) { //消えるアニメーションがキャンセルされたとき
+        // },
+        // leaveCancelled(el) { //消えるアニメーションがキャンセルされたとき
 
-        },
+        // },
     }
 }
 </script>
