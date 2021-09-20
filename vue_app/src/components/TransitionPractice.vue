eb026e2a6af20613e9004f4b0b5f57c911d564ee
こた <tokyosato@gmail.com>
2021-09-20 10:36:37 +0900

    表示切り替え

--- vue_app/src/components/TransitionPractice.vue
+++ vue_app/src/components/TransitionPractice.vue
@@ -4,14 +4,18 @@
         <button @click="myAnimation = 'fade'">Fade</button>
         <p>{{myAnimation}}</p>
         <button @click="show = !show">切り替え</button>
-        <!-- animationとtransitionの効果時間が異なる時どちらに合わせるかをtypeで指定 -->
-        <transition :name="myAnimation" appear>
-            <p v-if="show">bye</p>
-        </transition>
         <!-- <transition name="v"> <-デフォルト -->
         <transition
             enter-active-class="animate__animated animate__bounce" leave-active-class="animate__animated animate__shakeX" appear>
             <h4 v-if="show">Hello</h4>
+        </transition>
+        <!-- animationとtransitionの効果時間が異なる時どちらに合わせるかをtypeで指定 -->
+        <transition :name="myAnimation" appear>
+            <p v-if="show">bye</p>
+        </transition>
+        <transition :name="myAnimation">
+            <p v-if="show" key="bye">さよなら</p>
+            <p v-if="!show" key="hello">こんにちは</p>
         </transition>
     </div>
 </template>
@@ -68,7 +72,7 @@ export default {
     }
     .main {
         width: 70%;
-        margin: 3vh auto;
+        margin: 3vh auto 20vh;
         text-align: center;
     }
 </style>
