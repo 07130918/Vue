25645a612d32d85ec01d9794057eb4fac5733523
こた <tokyosato@gmail.com>
2021-09-20 11:06:04 +0900

    雛形作成

--- vue_app/src/components/TransitionPractice.vue
+++ vue_app/src/components/TransitionPractice.vue
@@ -4,6 +4,21 @@
         <button @click="myAnimation = 'fade'">Fade</button>
         <p>{{myAnimation}}</p>
         <button @click="show = !show">切り替え</button>
+        <br><br>
+        <transition
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @after-enter="afterEnter"
+            @enter-cancelled="enterCancelled"
+
+            @before-leave="beforeLeave"
+            @leave="leave"
+            @after-leave="afterLeave"
+            @leave-cancelled="leaveCancelled"
+        >
+            <div class="circle" v-if="show"></div>
+        </transition>
+        <br>
         <!-- <transition name="v"> <-デフォルト -->
         <transition
             enter-active-class="animate__animated animate__bounce" leave-active-class="animate__animated animate__shakeX" appear>
@@ -42,52 +57,69 @@ export default {
     components: {
         ComponentA,
         ComponentB
+    },
+    methods: {
+        beforeEnter() {},
+        enter() {},
+        afterEnter() {},
+        enterCancelled() {},
+        beforeLeave() {},
+        leave() {},
+        afterLeave() {},
+        leaveCancelled() {},
     }
 }
 </script>
 
 <style scoped>
-    .fade-enter { /*現れるときの最初の状態*/
-        opacity: 0;
-    }
-    .fade-enter-active { /*現れるときのトランジションの状態*/
-        transition: opacity .3s;
-    }
-    .fade-enter-to { /*現れるときの最後の状態*/
-        opacity: 1;
-    }
-    .fade-leave { /*消えるときの最初の状態*/
-        opacity: 1;
-    }
-    .fade-leave-active { /*消えるときのトランジションの状態*/
-        transition: opacity .3s;
-    }
-    .fade-leave-to { /*消えるときの最後の状態*/
-        opacity: 0;
-    }
-    .slide-enter, .slide-leave-to {
-        opacity: 0;
-    }
-    .slide-enter-active{
-        animation: slide-in .5s;
-        transition: opacity .5s;
-    }
-    .slide-leave-active{
-        animation: slide-in .5s reverse;
-        transition: opacity .5s;
-    }
+.circle {
+    width: 200px;
+    height: 200px;
+    margin: auto;
+    background-color: deeppink;
+    border-radius: 100px;
+}
+.fade-enter { /*現れるときの最初の状態*/
+    opacity: 0;
+}
+.fade-enter-active { /*現れるときのトランジションの状態*/
+    transition: opacity .3s;
+}
+.fade-enter-to { /*現れるときの最後の状態*/
+    opacity: 1;
+}
+.fade-leave { /*消えるときの最初の状態*/
+    opacity: 1;
+}
+.fade-leave-active { /*消えるときのトランジションの状態*/
+    transition: opacity .3s;
+}
+.fade-leave-to { /*消えるときの最後の状態*/
+    opacity: 0;
+}
+.slide-enter, .slide-leave-to {
+    opacity: 0;
+}
+.slide-enter-active{
+    animation: slide-in .5s;
+    transition: opacity .5s;
+}
+.slide-leave-active{
+    animation: slide-in .5s reverse;
+    transition: opacity .5s;
+}
 
-    @keyframes slide-in {
-        from {
-            transform: translateX(100px);
-        }
-        to {
-            transform: translateX(0);
-        }
+@keyframes slide-in {
+    from {
+        transform: translateX(100px);
     }
-    .main {
-        width: 70%;
-        margin: 3vh auto 20vh;
-        text-align: center;
+    to {
+        transform: translateX(0);
     }
+}
+.main {
+    width: 70%;
+    margin: 3vh auto 20vh;
+    text-align: center;
+}
 </style>
