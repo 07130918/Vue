acc68d01f7706420f55c1c1d025efa42a06c14bd
こた <tokyosato@gmail.com>
2021-09-21 11:33:30 +0900

    userId をデータに

--- vue_app/src/views/Users.vue
+++ vue_app/src/views/Users.vue
@@ -2,12 +2,17 @@
     <div>
         <h3>Users</h3>
         <button @click="toHome">Homeのページへ行く</button>
-        <h1>User No. {{ $route.params.id }}</h1>
+        <h1>User No. {{ userId }}</h1>
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            userId: this.$route.params.id,
+        }
+    },
     methods: {
         toHome() {
             this.$router.push({path: "/home"});
