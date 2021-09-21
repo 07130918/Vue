dfac47a4776162fe98a6bf0c1060ad826fec9517
こた <tokyosato@gmail.com>
2021-09-21 11:17:00 +0900

    ボタンによるリンク変更

--- vue_app/src/views/Home.vue
+++ vue_app/src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
         <h3>Home</h3>
+        <button @click="toUsers">Usersのページへ行く</button>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        toUsers() {
+            this.$router.push('users');
+        }
+    }
+}
+</script>
--- vue_app/src/views/Users.vue
+++ vue_app/src/views/Users.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
         <h3>Users</h3>
+        <button @click="toHome">Homeのページへ行く</button>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        toHome() {
+            this.$router.push('home');
+        }
+    }
+}
+</script>
