2ddd7307d6fc4bda26906f8a836be5d99e922f2d
こた <tokyosato@gmail.com>
2021-09-21 11:04:15 +0900

    active-class exact

--- vue_app/src/App.vue
+++ vue_app/src/App.vue
@@ -1,9 +1,15 @@
 <template>
     <div style="width: 700px; margin: auto; padding-top: 50px;">
         <nav>
-            <router-link to="/index" class="link">Index -></router-link>
-            <router-link to="/home" class="link">Home -></router-link>
-            <router-link to="/users" class="link">Users -></router-link>
+            <router-link to="/index" class="link" active-class="link--active" exact>
+                Index ->
+            </router-link>
+            <router-link to="/home" class="link" active-class="link--active" exact>
+                Home ->
+            </router-link>
+            <router-link to="/users" class="link" active-class="link--active" exact>
+                Users ->
+            </router-link>
         </nav>
         <router-view></router-view>
     </div>
@@ -12,5 +18,8 @@
 <style scoped>
 .link {
     margin-right: 20px;
+}
+.link--active {
+    font-size: 24px;
 }
 </style>
