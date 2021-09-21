9f924a5c6febbb49c10c80ff9ffd4cf3dc5c7179
こた <tokyosato@gmail.com>
2021-09-21 10:57:33 +0900

    add style

--- vue_app/src/App.vue
+++ vue_app/src/App.vue
@@ -1,10 +1,16 @@
 <template>
     <div style="width: 700px; margin: auto; padding-top: 50px;">
         <nav>
-            <router-link to="/index">Index -></router-link>
-            <router-link to="/home">Home -></router-link>
-            <router-link to="/users">Users -></router-link>
+            <router-link to="/index" class="link">Index -></router-link>
+            <router-link to="/home" class="link">Home -></router-link>
+            <router-link to="/users" class="link">Users -></router-link>
         </nav>
         <router-view></router-view>
     </div>
 </template>
+
+<style scoped>
+.link {
+    margin-right: 20px;
+}
+</style>
