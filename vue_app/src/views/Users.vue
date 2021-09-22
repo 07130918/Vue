f4cff9fdf6023ae273aae4c214f647baffb16d4b
こた <tokyosato@gmail.com>
2021-09-22 10:20:46 +0900

    動的なurl

--- vue_app/src/views/Users.vue
+++ vue_app/src/views/Users.vue
@@ -7,6 +7,9 @@
         <router-link to="/users/2">ユーザー2</router-link>
         <hr>
         <h1>User No. {{ id }}</h1>
+        <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">
+            次のユーザーのプロフィール
+        </router-link>
         <router-view></router-view>
     </div>
 </template>
