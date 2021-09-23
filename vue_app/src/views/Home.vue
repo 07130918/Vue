662624482ea8775ed1901613a987fcee7258cc70
こた <tokyosato@gmail.com>
2021-09-23 12:16:24 +0900

    computedプロパティにget set

--- vue_app/src/views/Home.vue
+++ vue_app/src/views/Home.vue
@@ -4,6 +4,8 @@
         <button @click="toUsers">Usersのページへ行く</button>
         <p>doubleCount: {{ doubleCount }}</p>
         <p>tripleCount: {{ tripleCount }}</p>
+        <input type="text" v-model="message">
+        <p>{{ message }}</p>
     </div>
 </template>
 
@@ -16,7 +18,18 @@ export default {
     //     myComponentTripleCount: "tripleCount"
     // }),
     computed: {
-        ...mapGetters(["doubleCount", "tripleCount"])
+        ...mapGetters(["doubleCount", "tripleCount"]),
+        // message() {
+        //     return this.$store.getters.message;
+        // },
+        message: {
+            get () {
+                return this.$store.getters.message;
+            },
+            set(value) {
+                this.$store.dispatch("updateMessage", value);
+            }
+        }
     },
     // computed: {
     //     doubleCount() {
