bb1e0af7306ef417d6fcbf7e0e49a197c6e2315e
こた <tokyosato@gmail.com>
2021-09-23 11:42:35 +0900

    mapActions

--- vue_app/src/views/HeaderUsers.vue
+++ vue_app/src/views/HeaderUsers.vue
@@ -7,15 +7,18 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
+import { mapActions, mapMutations } from 'vuex';
+// import { mapActions } from 'vuex';
 
 export default {
     methods: {
         ...mapMutations(["increment"]),
         // dispatch, actionを使った書き方
+        // ...mapActions(["increment"])
         // increment() {
         //     this.$store.dispatch("increment", 2)
         // },
+
         // increment() {
         //     // commit()でmutationsにある関数を実行
         //     this.$store.commit('increment', 1);
