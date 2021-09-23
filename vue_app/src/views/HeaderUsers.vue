defae50a077dd505d671b72c699a87b5df9e9343
こた <tokyosato@gmail.com>
2021-09-23 11:26:19 +0900

    mapMutations

--- vue_app/src/views/HeaderUsers.vue
+++ vue_app/src/views/HeaderUsers.vue
@@ -2,17 +2,20 @@
     <div>
         <h2>UsersHeader</h2>
         <p>store.jsのstate.count up!</p>
-        <button @click=incrementStoreCount>+1</button>
+        <button @click=increment(1)>+1</button>
     </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
     methods: {
-        incrementStoreCount() {
-            // commit()でmutationsにある関数を実行
-            this.$store.commit('increment', 1);
-        }
+        ...mapMutations(["increment"]),
+        // increment() {
+        //     // commit()でmutationsにある関数を実行
+        //     this.$store.commit('increment', 1);
+        // }
     }
-}
+};
 </script>
