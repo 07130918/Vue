fa6595e7f199a92fc5462fd9381871acad0b504a
こた <tokyosato@gmail.com>
2021-09-23 12:35:33 +0900

    プロパティごとに切り分け

--- vue_app/src/store/index.js
+++ vue_app/src/store/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import count from './modules/count';
+import getters from './getters'
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex);
 
@@ -8,26 +11,32 @@ export default new Vuex.Store({
     state: {
         message: "",
     },
-    getters: {
-        message: state => state.message
-    },
-    mutations: {
-        updateMessage(state, newMessage) {
-            state.message = newMessage;
-        }
-    },
-    // dispatch, actionを使った書き方
-    // actions: {
-    //     increment(context, number) {
-    //         context.commit("increment", number)
-    //     }
-    // }
-    actions: {
-        updateMessage({ commit }, newMessage) {
-            commit("updateMessage", newMessage)
-        }
-    },
+    getters,
+    mutations,
+    actions,
     modules: {
         count
     }
+    // state: {
+    //     message: "",
+    // },
+    // getters: {
+    //     message: state => state.message
+    // },
+    // mutations: {
+    //     updateMessage(state, newMessage) {
+    //         state.message = newMessage;
+    //     }
+    // },
+    // // dispatch, actionを使った書き方
+    // // actions: {
+    // //     increment(context, number) {
+    // //         context.commit("increment", number)
+    // //     }
+    // // }
+    // actions: {
+    //     updateMessage({ commit }, newMessage) {
+    //         commit("updateMessage", newMessage)
+    //     }
+    // },
 });
