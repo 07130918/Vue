6209cf83d5777d72d6153cb6c08779f1d2b31474
こた <tokyosato@gmail.com>
2021-10-05 11:01:00 +0900

    を改変

--- vue_app/src/components/LikeNumber.vue
+++ vue_app/src/components/LikeNumber.vue
@@ -2,7 +2,7 @@
 <!-- template内は必ず1つの要素をルートとしなければならない -->
     <div>
         <p>いいね: {{ halfNumber }}</p>
-        <button @click="increment">+0.5</button>
+        <button @click="increment">+1</button>
     </div>
 </template>
 
@@ -24,10 +24,15 @@
             }
         },
         methods: {
+            inc_sub() {
+                console.log(this.$parent.$data);
+                console.log(this.$parent.$data.number);
+                return this.totalNumber + 1;
+            },
             increment() {
-                // 第1引数に任意の命名, (第2引数にデータ)
+                // 第1引数に任意の命名, (第2引数にデータ操作)
                 // $emitはカスタムイベントを作る
-                this.$emit("my-click", this.totalNumber + 1);
+                this.$emit("my-click", this.inc_sub());
             }
         }
     }
