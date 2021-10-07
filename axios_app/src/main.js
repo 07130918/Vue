f76ef514b147d3c52c877207b2640dcf0b78945f
こた <tokyosato@gmail.com>
2021-10-07 14:45:07 +0900

    axios.interceptors

--- axios_app/src/main.js
+++ axios_app/src/main.js
@@ -9,6 +9,36 @@ axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-htt
 // axios.defaults.headers.common['Authorization'] = 'tekitou';
 // axios.defaults.headers.get['Accept'] = 'app/json';
 
+// interceptorsでリクエストとthen(もしくはcatch)の間に処理を挟む
+// サーバーに送った段階で処理を挟むのがrequest
+// サーバーからのレスポンスが来たで処理を挟むのがresponse
+const interceptorsRequest = axios.interceptors.request.use(
+    config => {
+        console.log('request');
+        console.log(config);
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+const interceptorsResponse = axios.interceptors.response.use(
+    // App.vueのcreatedのthenのresponseと同じもの
+    response => {
+        console.log('response');
+        console.log(response);
+        return response;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+// ejectでuseの処理を無効化
+axios.interceptors.request.eject(interceptorsRequest);
+axios.interceptors.response.eject(interceptorsResponse);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
