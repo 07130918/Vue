97e9b3f940a8002e2f5fff78039b119bab50c786
こた <tokyosato@gmail.com>
2021-10-07 12:03:37 +0900

    axiosでpost

--- axios_app/src/App.vue
+++ axios_app/src/App.vue
@@ -6,17 +6,47 @@
         <br><br>
         <label for="comment">コメント:</label>
         <textarea id="comment" v-model="comment"></textarea>
+        <br><br>
+        <button @click="createComment">コメント送信</button>
         <h2>掲示板</h2>
 
     </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     data() {
         return {
             name: "",
             comment: "",
+        }
+    },
+    methods: {
+        createComment() {
+            // 第1引数: URL, 第2引数: データ, 第3引数: header等詳細
+            axios.post(
+                'https://firestore.googleapis.com/v1/projects/vuejs-http-b5001/databases/(default)/documents/comments',
+                {   // cloud firestoreでの書き方
+                    fields: {
+                        name: {
+                            stringValue:this.name,
+                        },
+                        commennt: {
+                            stringValue:this.comment,
+                        }
+                    }
+                },
+            ) //postメソッドはPromiseオブジェクトを返してくる
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+            this.name = '';
+            this.comment = '';
         }
     }
 }
