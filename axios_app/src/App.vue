29289885e48803a91f5b70d3783294e11d4f1577
こた <tokyosato@gmail.com>
2021-10-07 11:05:19 +0900

    css

---
+++ axios_app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Arial, Helvetica, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
