a97b42e2f425a49c955858f53a24ff058ae32a90
こた <tokyosato@gmail.com>
2021-10-07 11:10:36 +0900

    topページの下準備

--- axios_app/src/App.vue
+++ axios_app/src/App.vue
@@ -1,10 +1,24 @@
 <template>
+    <div id="app">
+        <h3>掲示板に投稿する</h3>
+        <label for="name">ニックネーム</label>
+        <input id="name" type="text" v-model="name">
+        <br><br>
+        <label for="comment">コメント:</label>
+        <textarea id="comment" v-model="comment"></textarea>
+        <h2>掲示板</h2>
 
+    </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            name: "",
+            comment: "",
+        }
+    }
 }
 </script>
 
