b8adb7924f33296cf3f30dde19745acb8d2b004e
こた <tokyosato@gmail.com>
2021-10-07 13:06:29 +0900

    axiosにheaderを追加する方法をコメント

--- axios_app/src/main.js
+++ axios_app/src/main.js
@@ -4,7 +4,10 @@ import axios from 'axios'
 
 Vue.config.productionTip = false
 
+// axiosのdefault設定はmain.jsで行う
 axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-http-b5001/databases/(default)/documents";
+// axios.defaults.headers.common['Authorization'] = 'tekitou';
+// axios.defaults.headers.get['Accept'] = 'app/json';
 
 new Vue({
   render: h => h(App),
