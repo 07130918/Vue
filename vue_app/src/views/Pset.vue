eba4bc8328999ca6f865341df3505847a3085c5e
こた <tokyosato1@gmail.com>
2021-12-14 16:09:12 +0900

    Pset1

--- vue_app/src/views/Pset.vue
+++ vue_app/src/views/Pset.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <h1>Pset</h1>
+        <h1>Pset1</h1>
+        <h3 @mousemove="displayMousePosition">マウスを乗せてください</h3>
+        <p>X: {{ x }}, Y: {{ y }}</p>
+
+        <h1>Pset2</h1>
         <!-- v-forを回すときはkey属性を忘れずに -->
         <input type="text" v-model="message">
         <h3>{{ message }}</h3>
@@ -12,13 +16,19 @@ export default {
     data() {
         return {
             message: "",
+            x: null,
+            y: null,
         }
     },
     // components: {
 
     // },
-    // methods: {
-
-    // },
+    methods: {
+        displayMousePosition: function(event) {
+            console.log(event);
+            this.x = event.clientX;
+            this.y = event.clientY;
+        }
+    },
 }
 </script>
