e73ee0dc8f0822e2a329293f84bee0e9c9dff1c4
こた <tokyosato1@gmail.com>
2021-12-14 15:21:05 +0900

    SlotPracticeに変更

---
+++ vue_app/src/IndexApp.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <!-- slotにSlotPractice内要素をぶちこめる 名前付きが好ましい -->
+        <SlotPractice headerText="hello">
+            <h5>私は名前指定がされていないデフォルトスロット、離れていてもh5とくっつけられてしまう</h5>
+            <template #[dynamicSlotName]="slotProps">
+                <h2>トータルの良いいね数</h2>
+                <h4>{{ slotProps }}</h4>
+                <h4>last name: {{ slotProps.user.lastName }}</h4>
+            </template>
+            <h5>デフォルトスロット</h5>
+            <template v-slot:num>
+                <h4>{{ number }}</h4>
+            </template>
+        </SlotPractice>
+        <!-- このように親から子に渡す値を定義する, 属性として渡してあげる -->
+        <!-- カスタムイベント名はケバブケースで -->
+        <LikeNumber :totalNumber="number" @my-click="incrementNumber" @my-popup="popup"></LikeNumber>
+        <LikeNumber v-bind:total-number="number" v-on:my-click="incrementNumber" @my-popup="popup"></LikeNumber>
+        <KeepAlive></KeepAlive>
+        <FilterPractice></FilterPractice>
+        <Form></Form>
+        <TransitionPractice></TransitionPractice>
+    </div>
+</template>
+
+<script>
+import SlotPractice from './components/SlotPractice.vue'
+import KeepAlive from './components/KeepAlive.vue'
+import FilterPractice from './components/FilterPractice.vue'
+import Form from './components/Form.vue'
+import TransitionPractice from './components/TransitionPractice.vue'
+
+    export default {
+        data() {
+            return{
+                number: 10,
+                dynamicSlotName: 'title',
+                message: '',
+            }
+        },
+        // ローカル登録
+        components: {
+            // SlotPractice: SlotPractice 短縮できる
+            SlotPractice,
+            KeepAlive,
+            FilterPractice,
+            Form,
+            TransitionPractice,
+        },
+        methods: {
+            // valには$emitの第2引数が来る
+            incrementNumber(val) {
+                this.number = val;
+            },
+            popup(value) {
+                this.message = value;
+                alert(this.message);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid red;
+    }
+</style>
---
+++ vue_app/src/views/Pset.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Pset</h1>
+        <!-- v-forを回すときはkey属性を忘れずに -->
+        <input type="text" v-model="message">
+        <h3>{{ message }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "",
+        }
+    },
+    // components: {
+
+    // },
+    // methods: {
+
+    // },
+}
+</script>
