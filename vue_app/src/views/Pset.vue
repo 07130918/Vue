3869924d0e0dbc246cb36cf9285e3bc2ee12683d
こた <tokyosato1@gmail.com>
2021-12-15 11:32:40 +0900

    feat:2つの引数を取るv-onを追加

--- vue_app/src/views/Pset.vue
+++ vue_app/src/views/Pset.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
         <h1>Pset1</h1>
-        <h3 @mousemove="displayMousePosition">マウスを乗せてください</h3>
-        <p>X: {{ x }}, Y: {{ y }}</p>
-
+        <h3 id="mouse-h3" @mousemove="displayMousePosition">マウスを乗せてください</h3>
+        <p>X: {{ xPosition }}, Y: {{ yPosition }}</p>
+        <button @click="confirmPageReload(ensureMsg, $event)">
+            Page Reload
+        </button>
+        <hr>
         <h1>Pset2</h1>
         <!-- v-forを回すときはkey属性を忘れずに -->
         <input type="text" v-model="message">
@@ -15,20 +18,34 @@
 export default {
     data() {
         return {
-            message: "",
-            x: null,
-            y: null,
+            message: '',
+            xPosition: null,
+            yPosition: null,
+            ensureMsg: 'ページをリロードしますか?',
         }
     },
     // components: {
 
     // },
     methods: {
+        // v-onで実行される関数は自動で$eventが渡される(DOMイベントを参照したい場合は仮引数eventで受けてあげる)
         displayMousePosition: function(event) {
             console.log(event);
-            this.x = event.clientX;
-            this.y = event.clientY;
-        }
+            this.xPosition = event.clientX;
+            this.yPosition = event.clientY;
+        },
+        confirmPageReload: function(ensureMsg, event) {
+            console.log(event);
+            if (confirm(ensureMsg)) {
+                location.reload();
+            }
+        },
     },
 }
 </script>
+
+<style scoped>
+#mouse-h3 {
+    background-color: #0085C9;
+}
+</style>
